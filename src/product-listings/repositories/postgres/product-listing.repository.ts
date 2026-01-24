6e263b43dacb332dec4d56c536f68ad2ecfa46c3
Mantrimas <mantrimas.kasparavicius@gmail.com>
2026-01-24 21:52:21 +0200

    feat: remove pagination

--- src/product-listings/repositories/postgres/product-listing.repository.ts
+++ src/product-listings/repositories/postgres/product-listing.repository.ts
@@ -1,5 +1,5 @@
 import { Repository } from "typeorm";
-import { ProductListingRepositoryInterface, PaginationOptions, SearchOptions } from "../product-listing-repository.interface";
+import { ProductListingRepositoryInterface, SearchOptions } from "../product-listing-repository.interface";
 import { ProductListing } from "src/product-listings/entities/product-listing.entity";
 
 export class ProductListingRepository implements ProductListingRepositoryInterface {
@@ -9,10 +9,7 @@ export class ProductListingRepository implements ProductListingRepositoryInterfa
         private readonly repository: Repository<ProductListing>
     ) {}
 
-    async findAll(options?: PaginationOptions): Promise<[ProductListing[] | null, number]> {
-        const limit = options?.limit ?? 20;
-        const offset = options?.offset ?? 0;
-
+    async findAll(): Promise<[ProductListing[] | null, number]> {
         const [results, totalCount] = await this.repository.findAndCount({
             relations: {
                 productPreset: {
@@ -21,17 +18,13 @@ export class ProductListingRepository implements ProductListingRepositoryInterfa
                     platform: true,
                     region: true
                 }
-            },
-            take: limit,
-            skip: offset,
+            }
         });
 
         return [results, totalCount];
     }
 
     async findByFuzzyGameName(payload: SearchOptions): Promise<[ProductListing[] | null, number]> {
-        const limit = payload.limit ?? 20;
-        const offset = payload.offset ?? 0;
         const searchTerm = payload.searchTerm;
 
         const queryBuilder = this.repository
@@ -56,8 +49,6 @@ export class ProductListingRepository implements ProductListingRepositoryInterfa
         queryBuilder
             .orderBy('name_similarity', 'DESC')
             .addOrderBy('game.name', 'ASC')
-            .take(limit)
-            .skip(offset);
 
         return queryBuilder.getManyAndCount();
     }
--- src/product-listings/repositories/product-listing-repository.interface.ts
+++ src/product-listings/repositories/product-listing-repository.interface.ts
@@ -1,15 +1,10 @@
 import { ProductListing } from "../entities/product-listing.entity";
 
-export interface PaginationOptions {
-    limit?: number;
-    offset?: number;
-}
-
-export interface SearchOptions extends PaginationOptions {
+export interface SearchOptions {
     searchTerm: string;
 }
 
 export interface ProductListingRepositoryInterface {
-    findAll(options?: PaginationOptions): Promise<[ProductListing[] | null, number]>;
+    findAll(): Promise<[ProductListing[] | null, number]>;
     findByFuzzyGameName(payload: SearchOptions): Promise<[ProductListing[] | null, number]>;
 }
